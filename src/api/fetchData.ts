e0ca65ad85d921514046fe20a965515454cd0e6a
Anya <anna.efimova@amo.tech>
2024-07-15 09:16:21 +0300

    added env

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,20 +1,17 @@
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFubmFAdGVzdC5jb20iLCJ1c2VySWQiOiI2NjkwNTY2YjY0Zjg0MDY4ZTQ3MmQ4ZjYiLCJpYXQiOjE3MjA3NjU2MzQsImV4cCI6MTcyMTYyOTYzNH0.F8Wes4iYOlF5K1Iv7kR6vmaviP-3VHp-8Hrrz007WjA';
-
 export async function getSubcategories() {
-  const res = await fetch(`https://nodejs-lists-app.vercel.app/subcategories`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}subcategories`, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
     },
   });
   return res.json();
 }
 
 export async function getCategories() {
-  const res = await fetch(`https://nodejs-lists-app.vercel.app/categories`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}categories`, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
       ContentType: 'application/json',
     },
   });
@@ -28,8 +25,3 @@ export async function makeCategoriesReqWithErrCatch() {
     console.log('error', e); // no error
   }
 }
-
-// {
-//   "email":"anna@test.com",
-//   "password":"hellothere"
-// }
