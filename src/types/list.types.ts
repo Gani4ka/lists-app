aac720708f302f20cff3ba1b5e616b6117b87e68
Anya <anna.efimova@amo.tech>
2024-08-31 19:03:18 +0200

    added item isdone, category color

--- src/types/list.types.ts
+++ src/types/list.types.ts
@@ -2,6 +2,7 @@ export interface CategoryType {
   _id: string;
   title: string;
   icon: string;
+  color: string;
 }
 
 export type CategoryCreateType = Omit<CategoryType, '_id'>;
--- src/utils/setRandomIconColor.ts
+++ src/utils/setRandomIconColor.ts
@@ -1,7 +1,7 @@
 import { ICON_COLORS } from '@app/constants/iconColors';
 
 // TODO add localhost saving or DB?
-export function setRandomIconColor() {
+export function setRandomIconColor(): string {
   const randomIndex = Math.floor(Math.random() * ICON_COLORS.length);
   return ICON_COLORS[randomIndex].color;
 }
