ad6c2df9562e7a4212cb0a757aa10bae6eceaa70
Anya <anna.efimova@amo.tech>
2024-09-07 23:46:35 +0200

    chenged updated items to update many

--- src/api/item.ts
+++ src/api/item.ts
@@ -44,6 +44,28 @@ export async function updateItem(
   }
 }
 
+export async function updateItemMany(
+  subCategoryId: string,
+  data: ItemType[]
+): Promise<{ items: ItemType[] } | undefined> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}subcategory-items/all-subcategory-items/${subCategoryId}`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    return res.json();
+  } catch (e) {
+    console.log('error', e);
+  }
+}
+
 export async function deleteItem(
   itemId: string
 ): Promise<{ item: ItemType } | undefined> {
