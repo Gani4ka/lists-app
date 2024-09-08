633b91d2771598a24e2373c71617bee78d795bfd
Natalya Suslova <natalya.suslova@gmail.com>
2024-09-08 14:11:06 +0200

    add colors

--- src/components/color-picker/ColorPicker.tsx
+++ src/components/color-picker/ColorPicker.tsx
@@ -2,7 +2,26 @@
 import React from 'react';
 import { CirclePicker, ColorResult } from 'react-color';
 import { Flex } from '@radix-ui/themes';
-
+const pickerColors = [
+  '#f44336',
+  '#e91e63',
+  '#9c27b0',
+  '#673ab7',
+  '#3f51b5',
+  '#2196f3',
+  '#03a9f4',
+  '#00bcd4',
+  '#009688',
+  '#4caf50',
+  '#8bc34a',
+  '#cddc39',
+  '#ffeb3b',
+  '#ffc107',
+  '#ff9800',
+  '#ff5722',
+  '#795548',
+  '#607d8b',
+];
 type Props = {
   setColor: (color: string) => void;
   color: string;
@@ -15,7 +34,11 @@ const ColorPicker = ({ color, setColor }: Props) => {
   return (
     <Flex direction={'column'} gap={'2rem'} align={'center'}>
       <label>Choose your color:</label>
-      <CirclePicker color={color} onChangeComplete={handleColorChange} />
+      <CirclePicker
+        color={color}
+        onChangeComplete={handleColorChange}
+        colors={pickerColors}
+      />
       <p>{color}</p>
     </Flex>
   );
