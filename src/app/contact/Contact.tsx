73438ec07b0861c6246e931a11049269000f4c8b
Natalya Suslova <natalya.suslova@gmail.com>
2024-09-09 12:56:52 +0200

    remove unused code

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -1,13 +1,10 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import ColorPicker from '@app/components/color-picker/ColorPicker';
 
 export default function Contact() {
   const [color, setColor] = useState<string>('');
 
-  useEffect(() => {
-    console.log('selected color:', color);
-  }, [color]);
   return <ColorPicker color={color} setColor={setColor} />;
 }
