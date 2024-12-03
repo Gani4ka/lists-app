c15c692c86be6ce2bc90907ebf7c26912bb551f9
Anya <anna.efimova@amo.tech>
2024-12-03 15:36:56 +0100

    added icon links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,10 +41,39 @@ function RootLayout({ children }: LayoutProps) {
       <Favicon />
       <link rel="manifest" href="/manifest.json"></link>
       <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/icon-96x96.png"
+      ></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/icons/icon-192x192.png"
+      ></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/icons/icon-512x512.png"
+      ></link>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      ></link>
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/icons/apple-touch-icon.png"
       ></link>
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="default"
+      ></meta>
       <body>
         <SubCategoryContextProvider>
           <Theme {...themeConfig}>
