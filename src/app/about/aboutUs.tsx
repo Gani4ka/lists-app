db16fe17d4e955181a8be6dbeded962ac07f15f8
Natalya Suslova <natalya.suslova@gmail.com>
2024-12-21 15:04:38 +0100

    show contact us an about us also for non auth users

--- src/app/about/aboutUs.tsx
+++ src/app/about/aboutUs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Heading } from '@radix-ui/themes';
 
 import Link from 'next/link';
 
@@ -6,9 +7,9 @@ import styles from './aboutUs.module.css';
 const AboutUs = () => {
   return (
     <div className={styles.container}>
-      <h1>About Listify</h1>
+      <Heading as="h1">About Listify</Heading>
       <section className={styles.section}>
-        <h2>Our Mission</h2>
+        <Heading as="h2">Our Mission</Heading>
         <p>
           At Listify, our mission is to make organization effortless. Whether
           you are managing personal tasks, professional projects, or shared
@@ -16,20 +17,21 @@ const AboutUs = () => {
         </p>
       </section>
       <section className={styles.section}>
-        <h2>Our Story</h2>
+        <Heading as="h2">Our Story</Heading>
         <p>
           Founded by a team of 2 passionate developers, Listify started as a
           simple idea to streamline to-do lists.
         </p>
       </section>
       <section className={styles.section}>
-        <h2>Our Features</h2>
+        <Heading as="h2">Our Features</Heading>
         <ul>
           <li>
             Easy-to-use interface for creating categories and subcategories.
           </li>
           <li>Choose your colors and icons</li>
           <li>Earn points by completing your lists</li>
+          <li>Use AI to generate your personal lists</li>
         </ul>
       </section>
       <div className={styles.link}>
